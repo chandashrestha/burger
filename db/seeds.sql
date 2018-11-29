f7c4743eb4a18fed6706aad449d44f0b92844b8b
Chanda Shrestha <chandashrestha89@gmail.com>
2018-11-28 22:24:35 -0600

    framework

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('chanda', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('yan', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('shah', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('stephen', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('ingrid', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('alex', false);
